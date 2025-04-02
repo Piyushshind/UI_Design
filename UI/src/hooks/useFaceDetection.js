bb0ccb46049d81d19e99375508b54ad430017bff
Piyush Shinde <piyushshinde.199@gmail.com>
2025-04-02 17:26:06 +0530

    ad

--- UI/src/hooks/useFaceDetection.js
+++ UI/src/hooks/useFaceDetection.js
@@ -37,7 +37,7 @@ const useFaceDetection = (webcamRef) => {
                 const landmarks = detections[0].landmarks;
                 const isHuman = isValidHumanFace(landmarks);
 
-                // console.log("detectionCount  ;- ", detectionCount, "isValidHumanFace :- ", isHuman);
+                // console.log("detectionCount  :- ", detectionCount, "isValidHumanFace :- ", isHuman);
                 if (isHuman) {
                     detectionCount++;
                 }
